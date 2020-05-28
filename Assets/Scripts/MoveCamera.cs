c7485295cb0e0799b5ee5cd9f8fb02809d461a82
Arjun <arjunm04@outlook.com>
2020-05-28 16:50:51 +0100

    v0.0.1 Multiplayer works, no server-side authentication

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class MoveCamera : NetworkBehaviour
+{
+    public Transform head;
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+
+        Camera.main.orthographic = false;
+        Camera.main.transform.SetParent(head.transform);
+        Camera.main.transform.localPosition = Vector3.zero;
+        Camera.main.transform.localEulerAngles = Vector3.zero;
+
+    }
+}
